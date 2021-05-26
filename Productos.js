0e0bcf83d010554891775a1103522659bbf12ff0
facugigant <77868358+facugigant@users.noreply.github.com>
2021-05-26 20:28:46 -0300

    Update Productos.js

--- Productos.js
+++ Productos.js
@@ -4,75 +4,75 @@ const baseDeDatos = [
           id: 1,
           nombre: 'Motherboard modelo 1',
           precio: 300,
-          imagen: '/Mi carrito/img/Motherboard.jpg'
+          imagen: 'img/Motherboard.jpg'
       },
       {
           id: 2,
           nombre: 'Motherboard modelo 2',
           precio: 225,
-          imagen: '/Mi carrito/img/Motherboard.jpg'
+          imagen: 'img/Motherboard.jpg'
       },
       {
           id: 3,
           nombre: 'Motherboard modelo 3',
           precio: 100,
-          imagen: '/Mi carrito/img/Motherboard.jpg'
+          imagen: 'img/Motherboard.jpg'
       },
     //   Placas de video
       {
           id: 4,
           nombre: 'GPU Nvidia',
           precio: 1200,
-          imagen: '/Mi carrito/img/Gpu.jpg'
+          imagen: 'img/Gpu.jpg'
       },
       {
           id: 5,
           nombre: 'GPU Aourus',
           precio: 850,
-          imagen: '/Mi carrito/img/Gpu.jpg'
+          imagen: 'img/Gpu.jpg'
       },
       {
           id: 6,
           nombre: 'GPU AMD',
           precio: 400,
-          imagen: '/Mi carrito/img/Gpu.jpg'
+          imagen: 'img/Gpu.jpg'
       },
     //   Memorias
     {
           id: 7,
           nombre: 'RAM Gama Alta',
           precio: 400,
-          imagen: '/Mi carrito/img/Ram.jpg'
+          imagen: 'img/Ram.jpg'
       },
       {
           id: 8,
           nombre: 'RAM Gama Media',
           precio: 250,
-          imagen: '/Mi carrito/img/Ram.jpg'
+          imagen: 'img/Ram.jpg'
       },
       {
           id: 9,
           nombre: 'RAM Gama Baja',
           precio: 100,
-          imagen: '/Mi carrito/img/Ram.jpg'
+          imagen: 'img/Ram.jpg'
       },
                   //   
     {
           id: 10,
           nombre: 'CPU Gama Alta',
           precio: 1000,
-          imagen: '/Mi carrito/img/Cpu.jpg'
+          imagen: 'img/Cpu.jpg'
       },
       {
           id: 11,
           nombre: 'CPU Gama Media',
           precio: 400,
-          imagen: '/Mi carrito/img/Cpu.jpg'
+          imagen: 'img/Cpu.jpg'
       },
       {
           id: 12,
           nombre: 'CPU Gama Baja',
           precio: 200,
-          imagen: '/Mi carrito/img/Cpu.jpg'
+          imagen: 'img/Cpu.jpg'
       },
   ];
