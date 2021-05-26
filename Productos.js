517cb8f5c7798a1382bca5f8a21b2d1ab83521d4
facugigant <77868358+facugigant@users.noreply.github.com>
2021-05-26 20:15:29 -0300

    1.0

---
+++ Productos.js
@@ -0,0 +1,78 @@
+const baseDeDatos = [
+    //   Motherboards
+  {
+          id: 1,
+          nombre: 'Motherboard modelo 1',
+          precio: 300,
+          imagen: '/Mi carrito/img/Motherboard.jpg'
+      },
+      {
+          id: 2,
+          nombre: 'Motherboard modelo 2',
+          precio: 225,
+          imagen: '/Mi carrito/img/Motherboard.jpg'
+      },
+      {
+          id: 3,
+          nombre: 'Motherboard modelo 3',
+          precio: 100,
+          imagen: '/Mi carrito/img/Motherboard.jpg'
+      },
+    //   Placas de video
+      {
+          id: 4,
+          nombre: 'GPU Nvidia',
+          precio: 1200,
+          imagen: '/Mi carrito/img/Gpu.jpg'
+      },
+      {
+          id: 5,
+          nombre: 'GPU Aourus',
+          precio: 850,
+          imagen: '/Mi carrito/img/Gpu.jpg'
+      },
+      {
+          id: 6,
+          nombre: 'GPU AMD',
+          precio: 400,
+          imagen: '/Mi carrito/img/Gpu.jpg'
+      },
+    //   Memorias
+    {
+          id: 7,
+          nombre: 'RAM Gama Alta',
+          precio: 400,
+          imagen: '/Mi carrito/img/Ram.jpg'
+      },
+      {
+          id: 8,
+          nombre: 'RAM Gama Media',
+          precio: 250,
+          imagen: '/Mi carrito/img/Ram.jpg'
+      },
+      {
+          id: 9,
+          nombre: 'RAM Gama Baja',
+          precio: 100,
+          imagen: '/Mi carrito/img/Ram.jpg'
+      },
+                  //   
+    {
+          id: 10,
+          nombre: 'CPU Gama Alta',
+          precio: 1000,
+          imagen: '/Mi carrito/img/Cpu.jpg'
+      },
+      {
+          id: 11,
+          nombre: 'CPU Gama Media',
+          precio: 400,
+          imagen: '/Mi carrito/img/Cpu.jpg'
+      },
+      {
+          id: 12,
+          nombre: 'CPU Gama Baja',
+          precio: 200,
+          imagen: '/Mi carrito/img/Cpu.jpg'
+      },
+  ];
